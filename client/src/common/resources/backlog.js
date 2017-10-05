ea92a0caf49bf69352f523fed0843550f2f9a847
drakesvoboda <drake@thinkmoncur.com>
2017-10-05 14:34:24 -0400

    Document formatting

--- client/src/common/resources/backlog.js
+++ client/src/common/resources/backlog.js
@@ -1,11 +1,11 @@
 angular.module('resources.productbacklog', ['mongolabResource']);
 
 angular.module('resources.productbacklog').factory('ProductBacklog', ['mongolabResource', function (mongolabResource) {
-  var ProductBacklog = mongolabResource('productbacklog');
+	var ProductBacklog = mongolabResource('productbacklog');
 
-  ProductBacklog.forProject = function (projectId) {
-    return ProductBacklog.query({projectId:projectId});
-  };
+	ProductBacklog.forProject = function (projectId) {
+		return ProductBacklog.query({ projectId: projectId });
+	};
 
-  return ProductBacklog;
+	return ProductBacklog;
 }]);
--- client/src/common/resources/projects.js
+++ client/src/common/resources/projects.js
@@ -1,49 +1,49 @@
- angular.module('resources.projects', ['mongolabResource']);
+angular.module('resources.projects', ['mongolabResource']);
 angular.module('resources.projects').factory('Projects', ['mongolabResource', function ($mongolabResource) {
 
-  var Projects = $mongolabResource('projects');
+	var Projects = $mongolabResource('projects');
 
-  Projects.forUser = function(userId, successcb, errorcb) {
-	  return Projects.all().then(function (result) {
-		  return result.filter(function (x) {
-			  return x.isDevTeamMember(userId) || x.isScrumMaster(userId) || x.isProductOwner(userId);
-		  });
-	  })
-  };
+	Projects.forUser = function (userId, successcb, errorcb) {
+		return Projects.all().then(function (result) {
+			return result.filter(function (x) {
+				return x.isDevTeamMember(userId) || x.isScrumMaster(userId) || x.isProductOwner(userId);
+			});
+		})
+	};
 
-  Projects.prototype.isProductOwner = function (userId) {
-    return this.productOwner === userId;
-  };
-  Projects.prototype.canActAsProductOwner = function (userId) {
-    return !this.isScrumMaster(userId) && !this.isDevTeamMember(userId);
-  };
-  Projects.prototype.isScrumMaster = function (userId) {
-    return this.scrumMaster === userId;
-  };
-  Projects.prototype.canActAsScrumMaster = function (userId) {
-    return !this.isProductOwner(userId);
-  };
-  Projects.prototype.isDevTeamMember = function (userId) {
-    return this.teamMembers.indexOf(userId) >= 0;
-  };
-  Projects.prototype.canActAsDevTeamMember = function (userId) {
-    return !this.isProductOwner(userId);
-  };
+	Projects.prototype.isProductOwner = function (userId) {
+		return this.productOwner === userId;
+	};
+	Projects.prototype.canActAsProductOwner = function (userId) {
+		return !this.isScrumMaster(userId) && !this.isDevTeamMember(userId);
+	};
+	Projects.prototype.isScrumMaster = function (userId) {
+		return this.scrumMaster === userId;
+	};
+	Projects.prototype.canActAsScrumMaster = function (userId) {
+		return !this.isProductOwner(userId);
+	};
+	Projects.prototype.isDevTeamMember = function (userId) {
+		return this.teamMembers.indexOf(userId) >= 0;
+	};
+	Projects.prototype.canActAsDevTeamMember = function (userId) {
+		return !this.isProductOwner(userId);
+	};
 
-  Projects.prototype.getRoles = function (userId) {
-    var roles = [];
-    if (this.isProductOwner(userId)) {
-      roles.push('PO');
-    } else {
-      if (this.isScrumMaster(userId)){
-        roles.push('SM');
-      }
-      if (this.isDevTeamMember(userId)){
-        roles.push('DEV');
-      }
-    }
-    return roles;
-  };
+	Projects.prototype.getRoles = function (userId) {
+		var roles = [];
+		if (this.isProductOwner(userId)) {
+			roles.push('PO');
+		} else {
+			if (this.isScrumMaster(userId)) {
+				roles.push('SM');
+			}
+			if (this.isDevTeamMember(userId)) {
+				roles.push('DEV');
+			}
+		}
+		return roles;
+	};
 
-  return Projects;
+	return Projects;
 }]);
